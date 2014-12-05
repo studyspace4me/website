c918149f002f14dc57efa87667598be41da09e48
omar <omar.mahili@hotmail.it>
2014-12-05 17:28:09 +0100

    improved the off canvas menu visual style by changing colors, shadows and so on...

--- website/static/js/generic.js
+++ website/static/js/generic.js
@@ -1,23 +1,23 @@
 $(document).foundation();
 
 $(function() {
-    $("#loginSubmit").click( function(event){
+    //SUBMIT FORMS
+    $("#loginSubmit").click( function(){
         $( "#loginForm" ).submit();
     });
 
-    $("#signupSubmit").click( function(event){
+    $("#signupSubmit").click( function(){
         $( "#signupForm" ).submit();
     });
 
-    $("#preferencesSubmit").click( function(event){
+    $("#preferencesSubmit").click( function(){
         $( "#preferencesForm" ).submit();
     });
 
     //MECHANICS ANIMATIONS
-
     $("#contentResult").hide();
     $("#mapContainer").hide();
-    $("#headerResult").click( function(event){
+    $("#headerResult").click( function(){
         $("#contentResult").slideToggle();
         if($("#headerButton").text() == "More info"){
             $("#headerButton").text("Less info");
@@ -29,38 +29,11 @@ $(function() {
         }
     });
 
-    $("#mapButton").click( function(event){
+    $("#mapButton").click( function(){
         $("#mapContainer").slideToggle();
         if($("#mapButton").text() == "Click to view")
-        //to perserve style
             $("#mapButton").html($("#mapButton").html().replace("Click to view", "Click to hide"));
         else
             $("#mapButton").html($("#mapButton").html().replace("Click to hide", "Click to view"));
     });
-
-    //ORDERBY CLICK
-
-    /*
-    $("#orderbyContent .item").click( function(event){
-        normalBackground();
-
-        if(event.target.id == "all")
-            $("#orderbyContent #all").css("background-color", "rgb(0, 140, 186)");
-        if(event.target.id == "nearest")
-            $("#orderbyContent #nearest").css("background-color", "rgb(0, 140, 186)");
-        if(event.target.id == "temperature")
-            $("#orderbyContent #temperature").css("background-color", "rgb(0, 140, 186)");
-        if(event.target.id == "crowding")
-            $("#orderbyContent #crowding").css("background-color", "rgb(0, 140, 186)");
-        if(event.target.id == "favorites")
-            $("#orderbyContent #favorites").css("background-color", "rgb(0, 140, 186)");
-    });
-
-    function normalBackground() {
-        $("#orderbyContent #all").css("background-color", "transparent");
-        $("#orderbyContent #nearest").css("background-color", "transparent");
-        $("#orderbyContent #temperature").css("background-color", "transparent");
-        $("#orderbyContent #crowding").css("background-color", "transparent");
-        $("#orderbyContent #favorites").css("background-color", "transparent");
-    }*/
 });
