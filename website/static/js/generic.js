44bc4d470984f896083b4882daf1f420e5ddcf73
omar <omar.mahili@hotmail.it>
2014-12-11 19:33:39 +0100

    added favorite option on the header of the given results, improved the visual style of the result header on smaller width resolutions

--- website/static/js/generic.js
+++ website/static/js/generic.js
@@ -21,13 +21,24 @@ $(function() {
         $("#contentResult").slideToggle();
         //$("#headerResult .summaryHeader").hide();
         //$("#headerResult .summaryHeader").show();
+    }).on('click', '.favoriteStar', function(e){
+        //stop the propagation of the click on the favorite star, so when clicked the result content doesn't toggle
+        e.stopPropagation();
     });
 
-    $("#mapButton").click( function(){
+    $("#mapButton").on('click', function(e) {
         $("#mapContainer").slideToggle();
-        if($("#mapButton").text() == "Click to view")
+        if ($("#mapButton").text() == "Click to view")
             $("#mapButton").html($("#mapButton").html().replace("Click to view", "Click to hide"));
         else
             $("#mapButton").html($("#mapButton").html().replace("Click to hide", "Click to view"));
+    });
+
+    $("#favoriteResult").click( function(){
+        var srcImage = $("#favoriteResult").attr('src');
+        if(srcImage.indexOf("emptyFavorite") >= 0)
+            $("#favoriteResult").attr('src', 'static/assets/icons/favorite.svg');
+        else
+            $("#favoriteResult").attr('src', 'static/assets/icons/emptyFavorite.svg');
     });
 });
