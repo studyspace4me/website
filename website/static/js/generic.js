2d3fac4d8708491a008e8841e6ef0370219e8a1e
omar <omar.mahili@hotmail.it>
2014-12-13 01:19:33 +0100

    fixed visual bug when the browser window is small, improved the visual style of the contribute, preferences and terms of use pages, deleted some unneeded scss/css files, minor bug fixes

--- website/static/js/generic.js
+++ website/static/js/generic.js
@@ -1,31 +1,14 @@
 $(document).foundation();
 
 $(function() {
-    //SUBMIT FORMS
-    $("#loginSubmit").click(function () {
-        $("#loginForm").submit();
-    });
-
-    $("#signupSubmit").click(function () {
-        $("#signupForm").submit();
-    });
-
-    $("#preferencesSubmit").click(function () {
-        $("#preferencesForm").submit();
-    });
-
     //MECHANICS ANIMATIONS
     $("#contentResult").hide();
     $("#mapContainer").hide();
     $("#headerResult").click(function () {
-        if ($("#contentResult").is(":hidden")) {
-            //$("#headerResult .summaryHeader").hide();
+        if ($("#contentResult").is(":hidden"))
             $("#contentResult").slideDown();
-        }
-        else {
-            //$("#headerResult .summaryHeader").show();
+        else
             $("#contentResult").slideUp();
-        }
 
     }).on('click', '.favoriteStar', function (e) {
         //stop the propagation of the click on the favorite star, so when clicked the result content doesn't toggle
