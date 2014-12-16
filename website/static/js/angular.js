847f7a07231e404db69fa157c8f03ae0df602fc3
omar <omar.mahili@hotmail.it>
2014-12-16 05:15:22 +0100

    added angularjs to the iconbar, added visual style when selected a filter/orderby on the iconbar and made some style fixes

--- website/static/js/angular.js
+++ website/static/js/angular.js
@@ -6,52 +6,81 @@ customInterpolationApp.config(function($interpolateProvider) {
 });
 
 function dashboardController($http, $scope) {
-    $http.get("/api/rooms/").success(function (data) {
-        $scope.results = data;
-
-        $scope.getBackground = function (feedback) {
-            if (feedback == "Full")
-                return { 'background-color': 'red' };
-            else if (feedback == "Half")
-                return { 'background-color': 'orange' };
-            else if (feedback == "Empty")
-                return  { 'background-color': 'green' };
-
-            return  { 'background-color': 'transparent' };
-        };
-        $scope.getLastUpdate = function (lastUpdate) {
-            return "Updated " + moment(lastUpdate).fromNow();
-        };
-        $scope.getTime = function (until) {
-            return "Next lecture " + moment(until).fromNow();
-        };
-        $scope.getFavoriteImage = function (favorite) {
-            if (favorite == true)
-                return "static/assets/icons/favorite.svg";
-
-            return "static/assets/icons/emptyFavorite.svg";
-        };
-        $scope.getRoomIcon = function(type){
-            return "static/assets/icons/" + type + ".svg";
-        };
-        $scope.showContent = function (id) {
-            if ($("#" + id).is(":hidden")) {
-                $(".contentPanel").hide('fast');
-                $("#" + id).slideDown();
+    $scope.fetch = function(type) {
+        $http.get("/api/rooms/").success(function (data) {
+            if(type == 'all') {
+                $scope.results = data;
             }
-            else
-                $("#" + id).slideUp();
+            else if(type == 'favorites'){
 
-            getMap(id);
-            Swap("map" + id);
-            google.maps.event.trigger(mapV, "resize"); // resize map
-        };
-        $scope.changeFavorite = function (id) {
-            var srcImage = $("#" + id).attr('src');
-            if (srcImage.indexOf("emptyFavorite") >= 0)
-                $("#" + id).attr('src', 'static/assets/icons/favorite.svg');
-            else
-                $("#" + id).attr('src', 'static/assets/icons/emptyFavorite.svg');
-        };
-    });
+            }
+            else if(type == 'nearest'){
+
+            }
+            else if(type == 'crowding'){
+
+            }
+
+            //highlight the selected filter/orderby option
+            $('.iconStatus').css('background-color', 'transparent');
+            $('#' + type).css('background-color', 'orange');
+
+            $scope.getBackground = function (feedback) {
+                if (feedback == "Full")
+                    return { 'background-color': 'red' };
+                else if (feedback == "Half")
+                    return { 'background-color': 'orange' };
+                else if (feedback == "Empty")
+                    return  { 'background-color': 'green' };
+
+                return  { 'background-color': 'transparent' };
+            };
+            $scope.getLastUpdate = function (lastUpdate) {
+                return "Updated " + moment(lastUpdate).fromNow();
+            };
+            $scope.getTime = function (until) {
+                return "Next lecture " + moment(until).fromNow();
+            };
+            $scope.getFavoriteImage = function (favorite) {
+                if (favorite == true)
+                    return "static/assets/icons/favorite.svg";
+
+                return "static/assets/icons/emptyFavorite.svg";
+            };
+            $scope.getRoomIcon = function (type) {
+                return "static/assets/icons/" + type + ".svg";
+            };
+            $scope.showContent = function (id) {
+                if ($("#" + id).is(":hidden")) {
+                    $(".contentPanel").hide('fast');
+                    $("#" + id).slideDown();
+                }
+                else
+                    $("#" + id).slideUp();
+
+                getMap(id);
+                Swap("map" + id);
+                google.maps.event.trigger(mapV, "resize"); // resize map
+            };
+            $scope.changeFavorite = function (id) {
+                var srcImage = $("#" + id).attr('src');
+                if (srcImage.indexOf("emptyFavorite") >= 0)
+                    $("#" + id).attr('src', 'static/assets/icons/favorite.svg');
+                else
+                    $("#" + id).attr('src', 'static/assets/icons/emptyFavorite.svg');
+            };
+            $scope.reload = function () {
+                //$route.reload();
+                alert('lol');
+            };
+            $scope.order = function () {
+
+            };
+        });
+    };
+
+    var initialize = function(){
+        $scope.fetch('all');
+    };
+    initialize();
 }
