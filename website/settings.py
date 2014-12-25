ea8c9acff9961d2a3ee7749bee8177c7211483f7
Francesco Zanini <francesco@zanini.me>
2014-12-25 16:01:13 +0100

    Working settings

--- website/settings.py
+++ website/settings.py
@@ -13,5 +13,7 @@ REDIS = {
 VCAP_SERVICES = os.getenv('VCAP_SERVICES')
 if VCAP_SERVICES is not None:
     VCAP_SERVICES = json.loads(VCAP_SERVICES)
-    MONGODB['url'] = '/'.join(VCAP_SERVICES['mongodb-2.4']['credentials']['url'].split('/')[:-1]) + '/'
-    MONGODB['db'] = VCAP_SERVICES['mongodb-2.4']['credentials']['db']
+    mongodb = VCAP_SERVICES['mongodb-2.4'][0]
+    MONGODB['url'] =mongodb['credentials']['url']
+    # MONGODB['url'] = '/'.join(VCAP_SERVICES['mongodb-2.4'][0]['credentials']['url'].split('/')[:-1]) + '/'
+    MONGODB['db'] = mongodb['credentials']['db']
